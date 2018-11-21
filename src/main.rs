c591b70c4711fc6e91e528585576a0f5102ff7bf
venkatesh <venkatesh@acko.com>
2018-11-21 14:51:06 +0530

    changes made

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use std::io::prelude::*;
 use std::fs::File;
+extern crate regex;
+use regex::Regex;
 
 fn main() -> std::io::Result<()> {
 
     println!("happy bday");
 
-    let mut buffer = File::create("foo.txt")?;
+    let mut a = 5;
+    while a != 0 {
+        a = a-1;
+        println!("{}",a);
+    };
 
-    buffer.write(b"hello_world")?;
+//    let a = String::from("venki12321");
+//    println!("{}",a.replace("ven", "hey"));
+//
+//    let re = Regex::new(r"[d-i]").unwrap();
+//    let result = re.replace_all("id: 3", "0");
+//    println!("{}", result);
+//
+//    let mut buffer = File::create("foo.txt")?;
+//
+//    buffer.write(b"hello_world")?;
     Ok(())
 }
 
